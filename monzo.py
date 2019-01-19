51f7dee65986e48d4ed8b0219459bc1fcd947fa7
Oliver Black <oliver.black.uk@gmail.com>
2019-01-19 14:44:29 +0000

    Successful API request

--- monzo.py
+++ monzo.py
@@ -7,14 +7,22 @@
 # Upload data - text & link in notes, (email screenshot as receipt?)
 
 # pipenv install requests
-import
+import requests
+
+ACCOUNT_ID = 'acc_00009RIBEdUZdFewsGVO7d'
+
+ACCESS_TOKEN = open('monzo-access-token','r').read()[:-1]
 
 def match_and_upload_receipt(price, date, text, link):
     #http "https://api.monzo.com/transactions" \
     "Authorization: Bearer $access_token" \
     "account_id==$account_id"
+    r = requests.get('https://api.monzo.com/transactions?expand[]=merchant&account_id='+ACCOUNT_ID, \
+                     headers={'Authorization': 'Bearer '+ACCESS_TOKEN})
+    print(r)
 
 
 
 
 # test
+match_and_upload_receipt(0,"hi","by","wy")
