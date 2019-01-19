537df24ac41b636ba9c7bf2101b697cd97d494f1
JamesHinshelwood <jameshinshelwood1@gmail.com>
2019-01-19 17:12:33 +0000

    Add email scraper

---
+++ email_scraper.py
@@ -0,0 +1,25 @@
+import re
+from datetime import datetime
+
+from bs4 import BeautifulSoup
+
+
+def scrape(message_id, mime_msg):
+    soup = None
+    if mime_msg.is_multipart():
+        for p in mime_msg.get_payload():
+            if '<html>' in str(p):
+                soup = BeautifulSoup(p.get_payload(decode=True), 'html.parser')
+    else:
+        soup = BeautifulSoup(mime_msg.get_payload(decode=True), 'html.parser')
+
+    monies = map(lambda s: float(re.search(r'£\d+\.\d+', s).group(0)[1:]), soup.find_all(string=re.compile(r'£\d+\.\d+')))
+    money = max(monies)
+
+    time = datetime.strptime(mime_msg['date'], '%a, %d %b %Y %H:%M:%S %z')
+
+    subject = mime_msg['subject']
+
+    email_link = "https://mail.google.com/mail/#inbox/{}".format(message_id)
+
+    return money, time, subject, email_link
