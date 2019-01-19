e552f26a4bf397c98d4b04df3ab8ea2feaafd1be
zacharyedwardbull <zacharyedwardbull@gmail.com>
2019-01-19 17:59:59 +0000

    Add additional files from monzo template

---
+++ fetch_emails.py
@@ -0,0 +1,62 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import json
+import email
+import base64
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    subject_keywords = ['order', 'receipt', 'booking']
+    query = 'subject:' + ' OR subject:'.join(subject_keywords)
+    print(query)
+    response = service.users().messages().list(userId='me', q=query).execute()
+    messages = []
+
+    if 'messages' in response:
+        messages.extend(response['messages'])
+
+    while 'nextPageToken' in response:
+        page_token = response['nextPageToken']
+        response = service.users().messages().list(userId='me',
+                                                   pageToken=page_token, q=query).execute()
+        messages.extend(response['messages'])
+
+    for message in messages:
+        message = service.users().messages().get(userId='me', id=message['id'], format='raw').execute()
+        print('Message snippet: %s' % message['snippet'])
+        msg_str = base64.urlsafe_b64decode(message['raw'])
+        mime_msg = email.message_from_bytes(msg_str)
+
+
+if __name__ == '__main__':
+    main()
