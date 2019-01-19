2cc9ce0ba9d83a49379c016179ba13a613f35d67
Oliver Black <oliver.black.uk@gmail.com>
2019-01-19 20:27:39 +0000

    It fuckin works m9

--- email_scraper.py
+++ email_scraper.py
@@ -8,16 +8,18 @@ def scrape(message_id, mime_msg):
     soup = None
     if mime_msg.is_multipart():
         for p in mime_msg.get_payload():
-            if '<html>' in str(p):
+            if p.get_content_maintype() == 'text':
                 soup = BeautifulSoup(p.get_payload(decode=True), 'html.parser')
     else:
         soup = BeautifulSoup(mime_msg.get_payload(decode=True), 'html.parser')
 
     monies = map(lambda s: float(re.search(r'£\d+\.\d+', s).group(0)[1:]), soup.find_all(string=re.compile(r'£\d+\.\d+')))
 
-    if len(list(monies)) < 1:
+    monies = list(monies)
+
+    if len(monies) < 1:
         print("No prices found")
-        return
+        return 1
 
     money = max(monies)
 
