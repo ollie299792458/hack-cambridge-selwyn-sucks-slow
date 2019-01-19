d76d12ac379d8f23dbc2adae7f84c072ba9c7702
Oliver Black <oliver.black.uk@gmail.com>
2019-01-19 15:30:28 +0000

    Candidate transactions found

--- monzo.py
+++ monzo.py
@@ -1,6 +1,7 @@
 # Contract
 # Give me a price, date, text, and email links
 # I upload these if they match a debit from the monzo account (date close and price match)
+# Fails in the event of multiple matches, returns false
 
 # Download transactions around the date of the data
 # Match price, if one match succeed, if multiple pick closest date, if still multiple fail
@@ -8,20 +9,38 @@
 
 # pipenv install requests
 import requests
+#
+import json
 
 ACCOUNT_ID = 'acc_00009RIBEdUZdFewsGVO7d'
 
 ACCESS_TOKEN = open('monzo-access-token','r').read()[:-1]
 
+
+def within(date, date):
+    pass
+
+
 def match_and_upload_receipt(price, date, text, link):
+    #get all transactions
     #http "https://api.monzo.com/transactions" \
     "Authorization: Bearer $access_token" \
     "account_id==$account_id"
     r = requests.get('https://api.monzo.com/transactions?expand[]=merchant&account_id='+ACCOUNT_ID, \
                      headers={'Authorization': 'Bearer '+ACCESS_TOKEN})
-    print(r)
+    print("Transaction get: "+str(r))
 
+    transactions = json.loads(r.text)["transactions"];
 
+    candidates = []
+
+    for transaction in transactions :
+        if (transaction["amount"] == price) :
+            print("Candidate transaction:"+json.dumps(transaction, index=1));
+            candidates.append(transaction)
+
+    #get all debits within 2 days of date
+    print(json.dumps(transactions, indent=4));
 
 
 # test
